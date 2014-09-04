7f50dae2da7469546bda3069e47f251caae6415d
mayurkhatri <mayurkhatri122@gmail.com>
2014-09-04 23:17:18 +0530

    File reading logic

--- lib/backup_bindaas.rb
+++ lib/backup_bindaas.rb
@@ -3,7 +3,11 @@ require "backup_bindaas/version"
 module BackupBindaas
   class Backup
     def backup
-      puts "HI"
+      puts "Jai maata di"
+      file = File.open("../../config/database.yml", "r");
+      data = file.read
+      file.close
+      puts data
     end
   end
 end
