16c2c63e4c4c4fee8ca164e5dedcd142abc791df
mayurkhatri <mayurkhatri122@gmail.com>
2014-09-05 16:38:04 +0530

    Mysql dump - fix2:check3

--- lib/backup_bindaas.rb
+++ lib/backup_bindaas.rb
@@ -3,9 +3,12 @@ require "active_record"
 module BackupBindaas
   class Backup
     def backup(args)
+      puts "In lib file"
+      puts args
       if args.eql?("dev") || args.eql?("development")
         # TODO run mysqldump command
         db_config = ActiveRecord::Base.configurations["development"]
+        puts "Printing db config"
         puts db_config
         sh "mysqldump -u #{db_config['username']} -p#{db_config['password']} #{db_config['database']} > ~/projects/indian_food/testbk.sql"
         puts "Backup successful"
